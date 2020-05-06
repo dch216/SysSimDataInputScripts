9cc1efc695fc26ac0e0bbe4ad612e681febca84d
dch216 <dch216@psu.edu>
2020-05-06 11:02:49 -0400

    Update Scripts to Create Catalogs

--- combine_dr25_gaia_tmass.jl
+++ combine_dr25_gaia_tmass.jl
@@ -5,11 +5,13 @@ using PyPlot
 kep_filename = joinpath(abspath(joinpath(dirname(Base.find_package("ExoplanetsSysSim")),"..")), "data\\inputs", "q1_q17_dr25_stellar.csv")
 gaia_filename = joinpath(abspath(joinpath(dirname(Base.find_package("ExoplanetsSysSim")),"..")), "data\\inputs", "gaiadr2_keplerdr25_crossref.csv")
 mast_filename = joinpath(abspath(joinpath(dirname(Base.find_package("ExoplanetsSysSim")),"..")), "data\\inputs", "KeplerMAST_TargetProperties.csv")
-stellar_catalog_file_out = joinpath(abspath(joinpath(dirname(Base.find_package("ExoplanetsSysSim")),"..")), "data", "q1q17_dr25_gaia_fgk.jld2")
+tmass_filename = joinpath(Pkg.dir("ExoplanetsSysSim"), "data", "Gaia_Kepler_2MASS_mags.csv")
+stellar_catalog_file_out = joinpath(abspath(joinpath(dirname(Base.find_package("ExoplanetsSysSim")),"..")), "data", "q1q17_dr25_gaia_m.jld2")
 
 kep_df = CSV.read(kep_filename)
 gaia_df = CSV.read(gaia_filename)
 mast_df = CSV.read(mast_filename)
+tmass_df = CSV.read(tmass_filename)
 
 dup_gaiaid = findall(nonunique(DataFrame(x = gaia_df[!,:source_id])))
 gaiaid_keep = trues(size(gaia_df,1))
@@ -26,6 +28,7 @@ gaia_df = gaia_df[mag_match,:]
 gaia_col = [:kepid,:source_id,:parallax,:parallax_error,:astrometric_gof_al,:astrometric_excess_noise_sig,:phot_g_mean_mag,:bp_rp,:a_g_val,:priam_flags,:teff_val,:teff_percentile_lower,:teff_percentile_upper,:radius_val,:radius_percentile_lower,:radius_percentile_upper,:lum_val,:lum_percentile_lower,:lum_percentile_upper]
 df = join(kep_df, gaia_df[!,gaia_col], on=:kepid)
 df = join(df, mast_df, on=:kepid)
+df = join(df, tmass_df, on=:source_id, makeunique=true)
 kep_df = nothing
 gaia_df = nothing
 
@@ -35,9 +38,13 @@ df[!,:teff] = df[!,:teff_val]
 df[!,:teff_err1] = df[!,:teff_percentile_upper].-df[!,:teff_val]
 df[!,:teff_err2] = df[!,:teff_percentile_lower].-df[!,:teff_val]
 select!(df, Not([:teff_val,:teff_percentile_upper,:teff_percentile_lower]))
-df[!,:radius_err1] = df[!,:radius_percentile_upper].-df[!,:radius_val]
-df[!,:radius_err2] = df[!,:radius_percentile_lower].-df[!,:radius_val]
-df[!,:radius] = df[!,:radius_val]
+for x in 1:length(df[!,:kepid])
+    if !isnan(df[x,:radius_val])
+        df[x,:radius_err1] = df[x,:radius_percentile_upper]-df[x,:radius_val]
+        df[x,:radius_err2] = df[x,:radius_percentile_lower]-df[x,:radius_val]
+        df[x,:radius] = df[x,:radius_val]
+    end
+end
 select!(df, Not([:radius_val,:radius_percentile_upper,:radius_percentile_lower]))
 
 not_binary_suspect = (df[!,:astrometric_gof_al] .<= 20) .& (df[!,:astrometric_excess_noise_sig] .<= 5)
@@ -54,7 +61,7 @@ for x in 1:length(df[!,:kepid])
          push!(astrometry_good, false)
      end
 end
-astrometry_good = astrometry_good .& (df[!,:parallax_error] .< 0.1*df[!,:parallax])
+astrometry_good = astrometry_good .& (df[!,:parallax_error] .< 0.3*df[!,:parallax])
 # planet_search = df[!,:kepmag] .<= 16.
 
 has_mass = .! (ismissing.(df[!,:mass]) .| ismissing.(df[!,:mass_err1]) .| ismissing.(df[!,:mass_err2]))
@@ -63,43 +70,59 @@ has_radius = .! (ismissing.(df[!,:radius]) .| ismissing.(df[!,:radius_err1]) .| 
 has_cdpp = .! (ismissing.(df[!,:rrmscdpp01p5]) .| ismissing.(df[!,:rrmscdpp02p0]) .| ismissing.(df[!,:rrmscdpp02p5]) .| ismissing.(df[!,:rrmscdpp03p0]) .| ismissing.(df[!,:rrmscdpp03p5]) .| ismissing.(df[!,:rrmscdpp04p5]) .| ismissing.(df[!,:rrmscdpp05p0]) .| ismissing.(df[!,:rrmscdpp06p0]) .| ismissing.(df[!,:rrmscdpp07p5]) .| ismissing.(df[!,:rrmscdpp09p0]) .| ismissing.(df[!,:rrmscdpp10p5]) .| ismissing.(df[!,:rrmscdpp12p0]) .| ismissing.(df[!,:rrmscdpp12p5]) .| ismissing.(df[!,:rrmscdpp15p0]))
 has_rest = .! (ismissing.(df[!,:dataspan]) .| ismissing.(df[!,:dutycycle]))
 has_limbd = .! (ismissing.(df[!,:limbdark_coeff1]) .| ismissing.(df[!,:limbdark_coeff2]) .| ismissing.(df[!,:limbdark_coeff3]) .| ismissing.(df[!,:limbdark_coeff4]))
+has_tmass = .! (ismissing.(df[!,:ks_msigcom]) .| ismissing.(df[!,:ks_m]) .| ismissing.(df[!,:j_m]))
 
 mast_cut =.&(df[!,:numLCEXqtrs].>0,df[!,:numLCqtrs].>4)
 
-is_usable = .&(has_radius, has_mass, has_rest, has_cdpp, mast_cut, astrometry_good, not_binary_suspect)
+is_usable = .&(has_rest, has_cdpp, mast_cut, astrometry_good, has_tmass)#, not_binary_suspect)
 
 df = df[findall(is_usable),:]
 println("Total stars (KOIs) with valid parameters = ", length(df[!,:kepid]), " (", sum(df[!,:nkoi]),")")
 
-fgk_color = (0.5 .<= df[!,:bp_rp] .<= 1.7)
-# FGK = find(fgk_color)
-df = df[findall(fgk_color),:]
-near_FGK_MS = []
-coeff = [2.5,-3.6,0.9]
-for i in 1:6
-    global near_FGK_MS = (log10.(df[!,:lum_val]).< map(x->polyval(Poly(coeff),x),df[!,:bp_rp]) .+ log10(1.75))
-    global coeff = poly_fit(df[near_FGK_MS,:bp_rp],log10.(df[near_FGK_MS,:lum_val]),2)
-end
-FGK = findall(near_FGK_MS)
+m_absg = df[!,:phot_g_mean_mag] - df[!,:a_g_val] + 5 + 5*log10.(df[!,:parallax]/1000.)
+m_absk = df[!,:ks_m] + 5 + 5*log10.(df[!,:parallax]/1000.)
 
-println("Total FGK stars (KOIs) with valid parameters = ", length(FGK), " (", sum(df[FGK, :nkoi]),")")
+m_absk_err = sqrt.(df[!,:ks_msigcom].^2 .+ ((5./(df[!,:parallax]*log(10))).^2 .* df[!,:parallax_error].^2))
+df[!,:ks_absm] = m_absk
+df[!,:ks_absm_err] = m_absk_err
+m_gcolor = (1.7 .<= df[!,:bp_rp])# .<= 5.0)
+m_tcolor = (0.617 + 0.162) .<= (df[!,:j_m] .- df[!,:ks_m])
+
+df[!,:radius] = 1.9515 - (0.352*m_absk) .+ (0.0168*m_absk.^2)
+m_rad_err = abs.((-0.352 + 2*0.0168*m_absk) .* m_absk_err)
+df[!,:radius_err1] = m_rad_err
+df[!,:radius_err2] = -m_rad_err
+
+df[!,:mass] = 0.5858 + (0.3872*m_absk) .- (0.1217*m_absk.^2) .+ (0.0106*m_absk.^3) .- (2.7262e-4*m_absk.^4)
+m_mass_err = abs.((0.3872 - (2*0.1217*m_absk) .+ (3*0.0106*m_absk.^2) .- (4*2.7262e-4*m_absk.^3)).*m_absk_err)
+df[!,:mass_err1] = m_mass_err
+df[!,:mass_err2] = -m_mass_err
+
+m_teff = df[!,:teff] .<= 4000
+m_logg = df[!,:logg] .> 3
+#m_teff = (2320 .<= df[!,:teff] .<= 3870)
+m_gmag = (7.55 .<= m_absg)# .<= 16.33)
+m_kmag = (4.8 .<= m_absk)
+
+M_samp = find(.&(m_tcolor,m_kmag))#, df[!,:radius] .< 2, df[!,:mass] .> 0.5))
+
+println("Total M stars (KOIs) with valid parameters = ", length(M_samp), " (", sum(df[M_samp, :nkoi]),")")
 
 plot_samp = sample(1:length(df[!,:kepid]), 5000, replace=false)
-FGK_samp = sample(FGK, 5000, replace=false)
-plt[:scatter](df[FGK_samp, :bp_rp], m_absg[FGK_samp], s=3, label = "FGK", color="red")
+plt[:scatter](df[M_samp, :bp_rp], m_absg[M_samp], s=3, label = "M", color="red")
 plt[:scatter](df[plot_samp, :bp_rp], m_absg[plot_samp], s=3, label = "All", color="black")
 
 plt[:ylabel](L"$M_G$")
 plt[:xlabel](L"$B_p - R_p$")
 plt[:ylim](reverse(plt[:ylim]()))
 plt[:legend]()
-#plt[:savefig]("fgk-dwarf_samp.png")
+#plt[:savefig]("m-dwarf_samp.png")
 
 # See options at: http://exoplanetarchive.ipac.caltech.edu/docs/API_keplerstellar_columns.html
 # TODO SCI DETAIL or IMPORTANT?: Read in all CDPP's, so can interpolate?
-symbols_to_keep = [ :kepid, :source_id, :mass, :mass_err1, :mass_err2, :radius, :radius_err1, :radius_err2, :dens, :dens_err1, :dens_err2, :teff, :bp_rp, :lum_val, :rrmscdpp01p5, :rrmscdpp02p0, :rrmscdpp02p5, :rrmscdpp03p0, :rrmscdpp03p5, :rrmscdpp04p5, :rrmscdpp05p0, :rrmscdpp06p0, :rrmscdpp07p5, :rrmscdpp09p0, :rrmscdpp10p5, :rrmscdpp12p0, :rrmscdpp12p5, :rrmscdpp15p0, :dataspan, :dutycycle, :limbdark_coeff1, :limbdark_coeff2, :limbdark_coeff3, :limbdark_coeff4, :contam]
+symbols_to_keep = [ :kepid, :source_id, :mass, :mass_err1, :mass_err2, :radius, :radius_err1, :radius_err2, :dens, :dens_err1, :dens_err2, :teff, :phot_g_mean_mag, :bp_rp, :j_m, :ks_m, :lum_val, :rrmscdpp01p5, :rrmscdpp02p0, :rrmscdpp02p5, :rrmscdpp03p0, :rrmscdpp03p5, :rrmscdpp04p5, :rrmscdpp05p0, :rrmscdpp06p0, :rrmscdpp07p5, :rrmscdpp09p0, :rrmscdpp10p5, :rrmscdpp12p0, :rrmscdpp12p5, :rrmscdpp15p0, :dataspan, :dutycycle, :limbdark_coeff1, :limbdark_coeff2, :limbdark_coeff3, :limbdark_coeff4, :contam]
 # delete!(df, [~(x in symbols_to_keep) for x in names(df)])    # delete columns that we won't be using anyway
-df = df[FGK, symbols_to_keep]
+df = df[M_samp, symbols_to_keep]
 tmp_df = DataFrame()
 for col in names(df)
     tmp_df[!,col] = collect(skipmissing(df[!,col]))
